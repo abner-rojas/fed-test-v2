d75d93e1dfad5c56a9f5377636da804eeabe90de
Abner Rojas <abner.rojas@144.studio>
2022-03-09 09:13:18 -0500

    finished test

---
+++ src/3-press-releases-done/js/main.js
@@ -0,0 +1,76 @@
+var prWidget = {
+    options: {
+        containerSelector: '.module-press-release',
+        template: (
+            '{{#.}}' +
+                '{{#filteredItems}}' +
+                '<div class="module-press-release_item">' +
+                    '<h2 class="module-press-release_headline">{{headline}}</h2>' +
+                    '<p class="module-press-release_date">{{date}}</p>' +
+                    '<p class="module-press-release_short-body">{{shortBody}}</p>' +
+                    '<a class="button" href="{{relatedDoc}}">Download PDF</a>' +
+                    '<a class="button" href="{{url}}">Read more</a>' +
+                '</div>' +
+                '{{/filteredItems}}' +
+            '{{/.}}'                    
+        )
+    },
+
+    init: function() {
+        var templateItems = this.beforeRenderItems(prContent)
+        this.renderPRs(templateItems || []);
+        this.complete();
+    },
+
+    beforeRenderItems(content){
+        content.filteredItems = [];
+        content.filteredItems = content.items.filter(item => !item.tags.includes('earnings'));
+        
+
+        // content.items.forEach(function (el, ind, arr) {
+        //     content.filteredItems.push(el)
+        // });
+
+        return content;
+    },
+
+    renderPRs: function(prItems) {
+        var inst = this,
+            options = inst.options;
+
+        $(options.containerSelector).html(Mustache.render(options.template, prItems));
+    },
+
+    complete: function() {
+        // Add Slick Slider here (https://kenwheeler.github.io/slick/)
+        $('.module-press-release').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  dots: true,
+                },
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  dots: true,
+                },
+              },
+    
+            ],
+          })
+    }
+};
+
+prWidget.init();
